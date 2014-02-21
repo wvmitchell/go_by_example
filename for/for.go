fc5c9885fb804a24877ad96abf5d2817d4d6c935
Will Mitchell <wvmitchell@gmail.com>
2014-02-21 15:36:34 -0700

    for loop with break

--- for/for.go
+++ for/for.go
@@ -4,17 +4,22 @@ import "fmt"
 
 func main(){
 
-  i := 0
-  for i <= 3 {
-    fmt.Println(i)
-    i++
-  }
+  //i := 0
+  //for i <= 3 {
+  //  fmt.Println(i)
+  //  i++
+  //}
 
-  for i := 0; i < 10; i++ {
-    if i%2 == 0 {
-      fmt.Println(i)
-    } else {
-      fmt.Println("it's odd brah")
-    }
+  //for i := 0; i < 10; i++ {
+  //  if i%2 == 0 {
+  //    fmt.Println(i)
+  //  } else {
+  //    fmt.Println("it's odd brah")
+  //  }
+  //}
+
+  for {
+    fmt.Println("one")
+    break
   }
 }
