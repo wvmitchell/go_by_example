49f4b96ef6a7dd4f80e64a8545d1160a2c24b139
Will Mitchell <wvmitchell@gmail.com>
2014-02-21 14:00:50 -0700

    different types of var assignment

--- variables/variables.go
+++ variables/variables.go
@@ -2,11 +2,21 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 
-  var a string = "first"
-  b := "second"
-  fmt.Println(a)
-  fmt.Println(b)
+	var a string = "first"
+	b := "second"
+	fmt.Println(a)
+	fmt.Println(b)
+
+	var d, e = 1, 2
+	fmt.Println(d + e)
+
+  var f int
+  fmt.Println(f)
+
+  g, h := "mongoose", 1
+
+  fmt.Println("There is", h, g)
 
 }
