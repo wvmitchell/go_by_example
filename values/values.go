86e99cd73130df0b847a048057abdf8d579151ec
Will Mitchell <wvmitchell@gmail.com>
2014-02-21 13:52:03 -0700

    added boolean values

--- values/values.go
+++ values/values.go
@@ -7,7 +7,10 @@ func main() {
 	fmt.Println("go" + "lang")
 
 	fmt.Println("1+1 = ", 1+1)
+  fmt.Println("7.0/3.0 = ", 7.0/3.0)
 
-	fmt.Println("7.0/3.0 = ", 7.0/3.0)
+  fmt.Println(true && false)
+  fmt.Println(true && true)
+  fmt.Println(false || true)
 
 }
