124733a3cee2eea73a2df2bd2c5e92e2bc83995d
Will Mitchell <wvmitchell@gmail.com>
2014-02-23 16:31:47 -0700

    if else use cases

--- if_else/if_else.go
+++ if_else/if_else.go
@@ -2,4 +2,20 @@ package main
 
 import "fmt"
 
+func main(){
 
+  if 7%2 != 0 {
+    fmt.Println("Odd...")
+  } else {
+    fmt.Println("Even...")
+  }
+
+  if i := 10; i<10 {
+    fmt.Println("One digit")
+  } else if i>10 {
+    fmt.Println("Two digits, bigger than 10")
+  } else {
+    fmt.Println("Exactly 10")
+  }
+
+}
