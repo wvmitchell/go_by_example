c156d3dbde8cbd4f7505190296bf5d29a4c29daa
Will Mitchell <wvmitchell@gmail.com>
2014-03-08 15:22:07 -0700

    working with variadic functions

---
+++ variadic_functions/variadic_functions.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func sum(nums...int) int {
+  total := 0
+  for _, num := range nums {
+    total += num
+  }
+  return total
+}
+
+func main(){
+  fmt.Println(sum(1,2,3,4,5))
+}
