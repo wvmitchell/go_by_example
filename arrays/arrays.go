36c82133909b0ed53e7c029913f4e96b1b80be5e
Will Mitchell <wvmitchell@gmail.com>
2014-03-08 14:21:11 -0700

    working with slices

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -4,27 +4,26 @@ import "fmt"
 
 func main() {
 
-  var a [5]int
-  fmt.Println("emp:", a)
+	var a [5]int
+	fmt.Println("emp:", a)
 
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
 
-  a[4] = 100
-  fmt.Println("set:", a)
-  fmt.Println("get:", a[4])
+	fmt.Println("length:", len(a))
 
-  fmt.Println("length:", len(a))
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("b:", b)
 
-  b := [5]int{1,2,3,4,5}
-  fmt.Println("b:", b)
+	var twoD [2][3]int
+	fmt.Println("twoD: ", twoD)
 
-  var twoD [2][3]int
-  fmt.Println("twoD: ", twoD)
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i * j
+		}
+	}
 
-  for i := 0; i<2; i++ {
-    for j := 0; j<3; j++ {
-      twoD[i][j] = i*j
-    }
-  }
-
-  fmt.Println("twoD: ", twoD)
+	fmt.Println("twoD: ", twoD)
 }
