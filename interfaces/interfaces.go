7eed2c7c20068fa30ef2a66224514b6954bd2de8
Will Mitchell <wvmitchell@gmail.com>
2014-03-11 15:31:49 -0600

    make use of interface, and remove pointer requirement from recievers

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -16,24 +16,32 @@ type circle struct {
   radius float64
 }
 
-func (s *square) area() float64 {
+func (s square) area() float64 {
   return s.width * s.height
 }
 
-func (s *square) perim() float64 {
+func (s square) perim() float64 {
   return s.width*2 + s.height*2
 }
 
-func (c *circle) area() float64 {
+func (c circle) area() float64 {
   return math.Pi * math.Pow(c.radius, 2)
 }
 
-func (c *circle) perim() float64 {
+func (c circle) perim() float64 {
   return 2 * math.Pi * c.radius
+}
+
+func measure(g geometery) {
+  fmt.Println("Shape:", g)
+  fmt.Println("Area:", g.area())
+  fmt.Println("Perimeter:", g.perim())
 }
 
 func main() {
   c := circle{5.0}
-  fmt.Println(c.area())
-  fmt.Println(c.perim())
+  s := square{10, 21}
+
+  measure(c)
+  measure(s)
 }
