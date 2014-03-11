48132a35e0c3ee36594a74703f172c32805d72d7
Will Mitchell <wvmitchell@gmail.com>
2014-03-11 15:09:34 -0600

    added square and circle structs

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -3,4 +3,15 @@ package main
 import "fmt"
 import "math"
 
+type geometery interface {
+  area() float64
+  perim() float64
+}
 
+type square struct {
+  width, height float64
+}
+
+type circle struct {
+  radius float64
+}
