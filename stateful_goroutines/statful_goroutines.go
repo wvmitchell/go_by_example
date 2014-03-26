f96e0fb794f78609972da1df30576aba2905b57b
Will Mitchell <wvmitchell@gmail.com>
2014-03-25 18:04:29 -0600

    added goroutine to select on read and write channels

--- stateful_goroutines/statful_goroutines.go
+++ stateful_goroutines/statful_goroutines.go
@@ -24,4 +24,19 @@ func main() {
 
   reads := make(chan *readOp)
   writes := make(chan *writeOp)
+
+  go func() {
+    var state = make(map[int][int])
+
+    for {
+      select {
+        case read := <-reads: 
+          read.resp <- state[read.key]
+        case writes := <-writes:
+          state[write.key] = write.val
+          write.resp <- true
+        }
+      }
+    }
+  }
 }
