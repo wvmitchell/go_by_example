ff358be0f48dd657d456fa93a0b2209c6a2e749c
William Mitchell <wvmitchell@gmail.com>
2014-03-30 14:47:50 -0600

    added methods for creating and writing files

--- defer/defer.go
+++ defer/defer.go
@@ -5,10 +5,19 @@ import (
   "os"
 )
 
-funt main() {
+func main() {
 
   f := createFile("/tmp/defer.txt")
   defer closeFile(f)
   writeFile(f)
 
+}
+
+func createFile(p string) *os.File {
+  fmt.Println("Creating")
+  f, err := os.Create(p)
+  if err != nil {
+    panic(err)
+  }
+  return f
 }
