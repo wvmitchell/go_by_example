c3b8386650ec6013132dfa5c75ed7e9dbcb87b9c
William Mitchell <wvmitchell@gmail.com>
2014-04-01 19:17:39 -0600

    added function for determining if string is included in slice of strings

--- collection_functions/collection_functions.go
+++ collection_functions/collection_functions.go
@@ -6,7 +6,6 @@ import (
 )
 
 // Function to return first index of the target string
-
 func Index(vs []string, t string) int {
 	for i, v := range vs {
 		if v == t {
@@ -16,5 +15,19 @@ func Index(vs []string, t string) int {
 	return -1
 }
 
+// Function to return true if the target string is in the slice of strings
+func Include(vs []string, t string) bool {
+  for _, v := range vs {
+    if v == t {
+      return true
+    }
+  }
+  return false
+}
+
 func main() {
+  words := strings.Split("The quick brown fox jumps over the lazy dog", " ")
+  fmt.Println(Index(words, "fox"))
+  fmt.Println(Include(words, "boat"))
+  fmt.Println(Include(words, "brown"))
 }
