33fe3c043fa99375461f11c1dd2900063b3f2ded
William Mitchell <wvmitchell@gmail.com>
2014-04-01 19:13:02 -0600

    added function for finding index of string within array of strings

--- collection_functions/collection_functions.go
+++ collection_functions/collection_functions.go
@@ -1,10 +1,20 @@
 package main
 
 import (
-  "fmt"
-  "strings"
+	"fmt"
+	"strings"
 )
 
+// Function to return first index of the target string
+
+func Index(vs []string, t string) int {
+	for i, v := range vs {
+		if v == t {
+			return i
+		}
+	}
+	return -1
+}
+
 func main() {
-  fmt.Println(strings.Split("hello I am a cat", " "))
 }
