582c838ba591a620aa510b9a6b0aaf2c2549d314
William Mitchell <wvmitchell@gmail.com>
2014-04-03 11:16:12 -0600

    working with %v variants

--- string_formatting/string_formatting.go
+++ string_formatting/string_formatting.go
@@ -2,7 +2,24 @@ package main
 
 import (
   "fmt"
-  "strings"
+  //"os"
 )
 
 var pf = fmt.Printf
+
+type point struct {
+  x, y int
+}
+
+func main(){
+  p := point{1,3}
+
+  // => {1, 3}
+  pf("%v\n", p)
+
+  // => {x:1 y:3}
+  pf("%+v\n", p)
+
+  // => main.point{x:1, y:3}
+  pf("%#v\n", p)
+}
