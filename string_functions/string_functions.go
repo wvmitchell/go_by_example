848fae5d403552f8569a0416b5e8ac92d2719406
William Mitchell <wvmitchell@gmail.com>
2014-04-02 18:12:59 -0600

    use of multiple string methods

--- string_functions/string_functions.go
+++ string_functions/string_functions.go
@@ -9,4 +9,7 @@ var p = fmt.Println
 
 func main() {
 	p("Contains:", s.Contains("test", "es"))
+  p("Count:", s.Count("test", "t"))
+  p("HasPrefix:", s.HasPrefix("test", "te"))
+  p("HasSuffix:", s.HasSuffix("test", "st"))
 }
