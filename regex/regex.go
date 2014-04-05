7412ae5d6a62a2083719ac18d85de41e8710a624
William Mitchell <wvmitchell@gmail.com>
2014-04-05 15:48:19 -0600

    matching first string

--- regex/regex.go
+++ regex/regex.go
@@ -4,3 +4,32 @@ import (
   "regexp"
   "fmt"
 )
+
+var pf = fmt.Println
+
+func main() {
+
+  // returns true if string matches regexp
+  match, _ := regexp.MatchString("p([a-z]+)ch", "peach")
+  fmt.Println(match)
+
+  // more often, we'll compile a regexp for reuse
+  re, _ := regexp.Compile("p([a-z]+)ch")
+
+  // match test like earlier
+  pf(re.MatchString("peach"))
+
+  // find the first match
+  pf(re.FindString("peach punch"))
+
+  // also finds first match, but returns start and end indexes of string
+  pf(re.FindStringIndex("peach punch"))
+
+  // finds info for first match and first submatch
+  pf(re.FindStringSubmatch("peach punch"))
+
+  // finds indexes for first match and first submatch
+  pf(re.FindStringSubmatchIndex("peach punch"))
+
+
+}
