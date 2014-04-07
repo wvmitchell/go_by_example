ae2f3ce71409dd4ad9f32d4bdc6948550b498152
Will Mitchell <wvmitchell@gmail.com>
2014-04-07 14:02:01 -0600

    extracting info from time

--- time/time.go
+++ time/time.go
@@ -9,6 +9,22 @@ func main() {
 
   fp := fmt.Println
 
+  // current time
   now := time.Now()
   fp(now)
+
+  // build a time
+  then := time.Date(2009, 11, 17, 20, 34, 58, 8782832, time.UTC)
+  fp(then)
+
+  // extracting information
+  fp(then.Year())
+  fp(then.Month())
+  fp(then.Day())
+  fp(then.Weekday())
+  fp(then.Hour())
+  fp(then.Minute())
+  fp(then.Second())
+  fp(then.Nanosecond())
+  fp(then.Location())
 }
