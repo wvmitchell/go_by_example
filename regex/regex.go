2c642aad5dcc77024d36a247d819e53ce1866474
Will Mitchell <wvmitchell@gmail.com>
2014-04-07 10:45:22 -0600

    added ReplaceAllFunc

--- regex/regex.go
+++ regex/regex.go
@@ -3,6 +3,7 @@ package main
 import (
   "regexp"
   "fmt"
+  "bytes"
 )
 
 var pf = fmt.Println
@@ -31,5 +32,26 @@ func main() {
   // finds indexes for first match and first submatch
   pf(re.FindStringSubmatchIndex("peach punch"))
 
+  // All variants
+  pf(re.FindAllString("peach punch patch pinch", -1))
+  pf(re.FindAllStringSubmatch("peach pinch punch", -1))
+  pf(re.FindAllStringSubmatchIndex("peach punch pinch", -1))
+  pf(re.FindAllString("peach punch patch pinch", 2))
+
+
+  // byte variants
+  pf(re.Match([]byte("peach")))
+  pf(re.Find([]byte("peach punch patch pinch")))
+
+  // MustCompile for setting constants, only one return value, unlike Compile
+  re = regexp.MustCompile("p([a-z]+)ch")
+  pf(re)
+
+  pf(re.ReplaceAllString("A pretty peach", "pear"))
+
+  // ReplaceAllFunc allows transformation of matched text with function
+  in := []byte("a peach")
+  out := re.ReplaceAllFunc(in, bytes.ToUpper)
+  pf(string(out))
 
 }
