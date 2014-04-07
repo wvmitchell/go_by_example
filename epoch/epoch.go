c91e89fe62962a21533d897e26a93688f0ce3977
Will Mitchell <wvmitchell@gmail.com>
2014-04-07 16:53:35 -0600

    functions for working with unix epoch

--- epoch/epoch.go
+++ epoch/epoch.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func main() {
+
+  now := time.Now()
+
+  // seconds since unix epoch
+  secs := now.Unix()
+  fmt.Println(secs)
+
+  // nanoseconds since unix epoch
+  nsecs := now.UnixNano()
+  fmt.Println(nsecs)
+
+  // milliseconds since unix epoch
+  // there's no built-in function, so this must be done manually
+  msecs := nsecs / 1000000
+  fmt.Println(msecs)
+
+  fmt.Println(time.Unix(secs, 0))
+  fmt.Println(time.Unix(0, nsecs))
+
+}
