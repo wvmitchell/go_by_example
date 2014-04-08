45d40ef61d39f1f59e7d970787e3e000169c8d1d
Will Mitchell <wvmitchell@gmail.com>
2014-04-08 10:48:54 -0600

    working with reference based time formatting

--- parsing_and_formatting_time/parsing_and_formatting_time.go
+++ parsing_and_formatting_time/parsing_and_formatting_time.go
@@ -11,4 +11,10 @@ func main() {
   t := time.Now()
 
   p(t)
+
+  // reference based formatting
+  p(t.Format("2006-01-02T15:04:05Z07:00"))
+  p(t.Format("3:04PM"))
+  p(t.Format("Mon Jan _2 15:04:05 2006"))
+  p(t.Format("January 2 2006"))
 }
