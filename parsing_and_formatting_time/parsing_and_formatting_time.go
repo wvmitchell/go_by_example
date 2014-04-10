e3ce3e3a983e39330c2dea88f0a2cd6a6df0129a
Will Mitchell <wvmitchell@gmail.com>
2014-04-09 18:43:29 -0600

    added basic string formatting method

--- parsing_and_formatting_time/parsing_and_formatting_time.go
+++ parsing_and_formatting_time/parsing_and_formatting_time.go
@@ -17,4 +17,10 @@ func main() {
   p(t.Format("3:04PM"))
   p(t.Format("Mon Jan _2 15:04:05 2006"))
   p(t.Format("January 2 2006"))
+
+  // standard string formatting with extracted info
+  fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
+    t.Year(), t.Month(), t.Day(),
+    t.Hour(), t.Minute(), t.Second(),
+  )
 }
