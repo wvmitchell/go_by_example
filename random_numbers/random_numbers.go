b56b6c8fbb934f5bc19ea49598583381a3bcea29
Will Mitchell <wvmitchell@gmail.com>
2014-04-10 10:33:36 -0600

    generating with a custom seed'

--- random_numbers/random_numbers.go
+++ random_numbers/random_numbers.go
@@ -9,13 +9,30 @@ func main() {
 
   p := fmt.Println
 
+  // random ints
   p(rand.Intn(100))
   p(rand.Intn(100))
+  p()
 
+  // random floats
   p(rand.Float64())
   p(rand.Float64())
+  p()
 
+  // random floats in other ranges
   p(rand.Float64()+5)
   p(rand.Float64()*5+5)
+  p()
+
+  // new seed
+  s1 := rand.NewSource(50)
+  r1 := rand.New(s1)
+  p(r1.Intn(100))
+  p(r1.Intn(100))
+  p()
+
+  // new rand, with same seed, same output
+
+
 
 }
