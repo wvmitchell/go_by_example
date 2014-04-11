ec499558a1b62ef5aeb293ded22b04531965e204
Will Mitchell <wvmitchell@gmail.com>
2014-04-11 14:36:56 -0600

    working with number parsing

--- number_parsing/number_parsing.go
+++ number_parsing/number_parsing.go
@@ -13,8 +13,27 @@ func main() {
   f, _ := strconv.ParseFloat("2.323", 64)
   p(f)
 
-  // parse int
-  i, _ := strconv.ParseInt("234a", 12, 64)
+  // parse int (base value of 0 requires inference of base from string)
+  i, _ := strconv.ParseInt("234", 0, 64)
   p(i)
+
+  n, _ := strconv.ParseInt("-234", 0, 64)
+  p(n)
+
+  // parse hex
+  h, _ := strconv.ParseInt("0x2a3c4f", 0, 64)
+  p(h)
+
+  // parse uint
+  u, _ := strconv.ParseUint("483", 0, 64)
+  p(u)
+
+  // Atoi convinience method for base 10 numbers
+  t, _ := strconv.Atoi("43")
+  p(t)
+
+  // Atoi throws error on invalid input
+  _, e := strconv.Atoi("potato")
+  p(e)
 
 }
