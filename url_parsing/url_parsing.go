d07033edf6f908ac05997ff271ddaaba11c5bea1
Will Mitchell <wvmitchell@gmail.com>
2014-04-12 13:02:38 -0600

    simple parse of url and scheme

--- url_parsing/url_parsing.go
+++ url_parsing/url_parsing.go
@@ -2,4 +2,23 @@ package main
 
 import (
   "fmt"
+  "net/url"
+  //"strings"
 )
+
+func main() {
+
+  p := fmt.Println
+
+  s := "postgres://user:pass@host.com:5432/path?k=v#f"
+
+  u, err := url.Parse(s)
+
+  if err != nil {
+    panic(err)
+  }
+
+  p("url:", u)
+  p("scheme:", u.Scheme)
+
+}
