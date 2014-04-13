7607fd5db122fd92ab9d07b5aa7e8d66ab25e5d2
Will Mitchell <wvmitchell@gmail.com>
2014-04-13 12:36:03 -0600

    working with hashes

--- sha1_hashing/sha1_hashing.go
+++ sha1_hashing/sha1_hashing.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+  "crypto/sha1"
+  "fmt"
+)
+
+func main() {
+
+  p := fmt.Println
+
+  s := "Hash this string"
+
+  h := sha1.New()
+
+  h.Write([]byte(s))
+
+  bs := h.Sum(nil)
+
+  p(s)
+  fmt.Printf("%x\n", bs)
+
+}
