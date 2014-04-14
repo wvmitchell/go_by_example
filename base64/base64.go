37a2cfdf3dba5ecaa66e60ac4dde94a1a416b28c
Will Mitchell <wvmitchell@gmail.com>
2014-04-14 10:30:17 -0600

    url compatible encoding and decoding

--- base64/base64.go
+++ base64/base64.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 
+  // encoding and decoding a string
   p := fmt.Println
   data := "123abc!?$*&()'-=@~"
 
@@ -15,5 +16,12 @@ func main() {
 
   dEnc, _ := b64.StdEncoding.DecodeString(sEnc)
   p(string(dEnc))
+
+  // URL compatible base64 encoding and decoding
+  uEnc := b64.URLEncoding.EncodeToString([]byte(data))
+  p(uEnc)
+
+  duEnc, _ := b64.URLEncoding.DecodeString(uEnc)
+  p(string(duEnc))
 
 }
