a4cbb0a60b8b663a597b66ad4fc76551e53cc596
Will Mitchell <wvmitchell@gmail.com>
2014-04-16 11:13:56 -0600

    writing bytes to a file

--- writing_files/writing_files.go
+++ writing_files/writing_files.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+  //"bufio"
+  "fmt"
+  "io/ioutil"
+  "os"
+)
+
+func check(e error) {
+  if e != nil {
+    panic(e)
+  }
+}
+
+func main() {
+
+  // dump string into a file
+  d1 := []byte("hello\nhere is some\ngo")
+  err := ioutil.WriteFile("/tmp/dat1", d1, 0644)
+  check(err)
+
+  // more granular writes
+  f, err := os.Create("/tmp/dat2")
+  check(err)
+
+  // defer closing the file immediately after opening
+  defer f.Close()
+
+  // writing bytes to a file
+  d2 := []byte{115, 111, 109, 101, 10}
+  n2, err := f.Write(d2)
+  fmt.Printf("Wrote %d bytes\n", n2)
+
+
+
+}
