2b52f82ec766eaebac1be4d39fe5efc59346f4b2
Will Mitchell <wvmitchell@gmail.com>
2014-04-21 14:51:19 -0600

    print arguments

--- command_line_args/command_line_args.go
+++ command_line_args/command_line_args.go
@@ -4,3 +4,16 @@ import (
   "os"
   "fmt"
 )
+
+func main() {
+
+  // aruguments with name of program and path
+  argsWithProg := os.Args
+
+  // and without
+  argsWithoutProg := os.Args[1:]
+
+  fmt.Println(argsWithProg)
+  fmt.Println(argsWithoutProg)
+
+}
