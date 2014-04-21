707ba018d3a1f1489234be14f62d33b63563a289
Will Mitchell <wvmitchell@gmail.com>
2014-04-21 14:55:00 -0600

    access individual arguments

--- command_line_args/command_line_args.go
+++ command_line_args/command_line_args.go
@@ -13,7 +13,11 @@ func main() {
   // and without
   argsWithoutProg := os.Args[1:]
 
+  // individual argument
+  individual := os.Args[1]
+
   fmt.Println(argsWithProg)
   fmt.Println(argsWithoutProg)
+  fmt.Println(individual)
 
 }
