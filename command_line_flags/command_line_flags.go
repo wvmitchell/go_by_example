4ef7f6bd88cfcb0deabec936df26054df754a116
Will Mitchell <wvmitchell@gmail.com>
2014-04-22 17:15:57 -0600

    accepting string and int flags

--- command_line_flags/command_line_flags.go
+++ command_line_flags/command_line_flags.go
@@ -4,3 +4,16 @@ import (
   "flag"
   "fmt"
 )
+
+func main() {
+
+  // name of option, default value, and description
+  wordPtr := flag.String("word", "foo", "a string")
+
+  numPtr := flag.Int("num", 45, "an int")
+
+  flag.Parse()
+
+  fmt.Println("wordPtr", *wordPtr)
+  fmt.Println("numPtr", *numPtr)
+}
