01442b7b0a1cea5b42d0e6f279ab2f370106fcf0
Will Mitchell <wvmitchell@gmail.com>
2014-04-24 13:47:28 -0600

    added and used date command

--- spawning_processes/spawning_processes.go
+++ spawning_processes/spawning_processes.go
@@ -2,6 +2,20 @@ package main
 
 import (
   "fmt"
-  "io/ioutil"
+  //"io/ioutil"
   "os/exec"
 )
+
+func main() {
+
+  p := fmt.Println
+
+  dateCmd := exec.Command("date")
+
+  dateOut, err := dateCmd.Output()
+  if err != nil {
+    panic(err)
+  }
+  p("> date")
+  p(string(dateOut))
+}
