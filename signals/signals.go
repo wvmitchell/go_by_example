69377c50f3e98a6f28088e17030547e19c2379d0
Will Mitchell <wvmitchell@gmail.com>
2014-04-28 10:28:40 -0600

    sending interupting signal

--- signals/signals.go
+++ signals/signals.go
@@ -9,12 +9,22 @@ import (
 
 func main() {
 
+  p := fmt.Println
+
   sigs := make(chan os.Signal, 1)
   done := make(chan bool, 1)
 
   signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
   go func() {
+    sig := <-sigs
+    p()
+    p(sig)
+    done <- true
   }()
+
+  p("Awaiting signal")
+  <-done
+  p("Exiting")
 
 }
